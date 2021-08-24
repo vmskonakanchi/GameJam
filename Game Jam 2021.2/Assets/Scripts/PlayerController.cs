56c6063d05b1028cc2e83fe92aa57b6725540e55
Vamsi Konakanchi <vmskonakanchi@gmail.com>
2021-08-24 15:14:18 +0530

    Update PlayerController.cs

--- Game Jam 2021.2/Assets/Scripts/PlayerController.cs
+++ Game Jam 2021.2/Assets/Scripts/PlayerController.cs
@@ -7,38 +7,40 @@ public class PlayerController : MonoBehaviour
 {
     Rigidbody2D rb;
     SpriteRenderer sp;
+    public GameObject bulletPrefab;
+    public Transform playerbulletFirePoint;
+    public Transform groundCheckPoint;
     [SerializeField] float moveSpeed;
     [SerializeField] float jumpSpeed;
-    [SerializeField] int playerHP = 100;
+    //[SerializeField] int playerHP = 100;
+    [SerializeField] float launchForce;
+    [SerializeField] bool isOnGround;
+    [SerializeField] float groundHitRadius;
+
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
         sp = GetComponent<SpriteRenderer>();
     }
-
-    void FixedUpdate()
-    {
-        Move();
-        Jump();
-    }
     void Update()
     {
+        Move();
+        ChecKGround();
+        Jump();
         Shoot();
     }
-
-    void Jump()
+    void Shoot()
     {
-        //play jump sound
-        //play jump animations
-        //play particls if any
-        if(Input.GetKeyDown(KeyCode.Space))
+        //Instataite projectiles depending upon ammo type 
+        //play shoot sound
+        //play shoot animation
+        if(Input.GetMouseButtonDown(0))
         {
-            rb.AddForce (Vector2.up * jumpSpeed * Time.deltaTime , ForceMode2D.Impulse);
+            Vector2 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            GameObject newbullet = Instantiate(bulletPrefab , playerbulletFirePoint.position , transform.rotation);
+            newbullet.GetComponent<Rigidbody2D>().velocity =  mousePos * Vector2.right * transform.rotation *  launchForce;
         }
-    }   
-   void Shoot()
-    {
-        //Instataite projectiles
+        
     }
 
     void Move()
@@ -48,9 +50,37 @@ public class PlayerController : MonoBehaviour
         //play particls if any
         float horizontalInput = Input.GetAxis("Horizontal");
         rb.velocity = Vector2.right  * horizontalInput * moveSpeed * Time.deltaTime;  
-        if(horizontalInput == -1)
+        if(horizontalInput < 0)
         {
             sp.flipX = true;
         }   
+    }
+
+    void ChecKGround()
+    {
+        Collider2D groundHit = Physics2D.OverlapCircle(groundCheckPoint.position , groundHitRadius);
+        if( groundHit == true)
+        {
+            isOnGround = true;
+        }
+        else if (groundHit == false)
+        {
+            isOnGround = false;
+        }
+    }
+    void Jump()
+    {
+        //play jump sound
+        //play jump animations
+        //play particls if any
+        if (Input.GetKeyDown(KeyCode.Space) && isOnGround == true)
+        {
+            rb.velocity = Vector2.up * jumpSpeed * Time.deltaTime;
+        }
+        
+    }
+    void OnDrawGizmosSelected()
+    {
+        Gizmos.DrawWireSphere(groundCheckPoint.position , groundHitRadius);
     }
 }
