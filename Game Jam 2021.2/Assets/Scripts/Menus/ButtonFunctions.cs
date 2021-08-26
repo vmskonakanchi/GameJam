732c5edac833e71ea9d5cdc2be5f9f71cff3d222
sansuno68 <65071967+sansuno68@users.noreply.github.com>
2021-08-26 20:13:34 +0200

    MainMenu functions finished + settings in work

--- Game Jam 2021.2/Assets/Scripts/Menus/ButtonFunctions.cs
+++ Game Jam 2021.2/Assets/Scripts/Menus/ButtonFunctions.cs
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -34,3 +35,40 @@ public class ButtonFunctions : MonoBehaviour
         childImage.sprite = normalSprite;
     }
 }
+=======
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ButtonFunctions : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    
+    public GameObject highlightedSprite;
+
+    Image childImage;
+
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void ButtonHighlight()
+    {
+        highlightedSprite.SetActive(true);
+    }
+
+    public void NormalButtonState()
+    {
+        highlightedSprite.SetActive(false);
+    }
+}
+>>>>>>> Stashed changes
--- Game Jam 2021.2/Assets/Scripts/Menus/MainMenu.cs
+++ Game Jam 2021.2/Assets/Scripts/Menus/MainMenu.cs
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -46,3 +47,83 @@ public class MainMenu : MonoBehaviour
 
 
 }
+=======
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    [SerializeField] GameObject optionsScreen;
+    [SerializeField] GameObject titleScreen;
+    Transform currentScreen;
+
+    void Start()
+    {
+        currentScreen = GetComponent<Transform>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void Play()
+    {
+
+        //(Maybe add Buttons Animation)
+
+        //Get Player into next Scene
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    public void Options()
+    {
+
+        //Open new Options Menu
+        ShowNewScreen(optionsScreen);
+
+    }
+
+    public void Quit()
+    {
+
+        //(Maybe add Buttons Animation)
+
+        //Quits the Apllication
+        Application.Quit();
+    }
+
+    public void Menu()
+    {
+
+        //Open new Options Menu
+        ShowNewScreen(titleScreen);
+
+    }
+
+    void ShowNewScreen(GameObject screen)
+    {
+        currentScreen.GetComponent<Animator>().SetTrigger("SwipeOut");
+        screen.SetActive(true);
+        
+
+
+            
+
+    }
+
+    //Is an animation trigger (triggered by swipe out animation)
+    public void DisableGameobject()
+    {
+        currentScreen.gameObject.SetActive(false);
+    }
+
+
+
+}
+>>>>>>> Stashed changes
