aa5b985164af99a56c05cb9b6c3a746e43ceae05
sansuno68 <65071967+sansuno68@users.noreply.github.com>
2021-08-27 15:25:25 +0200

    Missile Error Fixed

--- Game Jam 2021.2/Assets/Scripts/Enemy/Missile.cs
+++ Game Jam 2021.2/Assets/Scripts/Enemy/Missile.cs
@@ -5,14 +5,31 @@ using UnityEngine;
 public class Missile : MonoBehaviour
 {
     // Start is called before the first frame update
+    Transform player;
+    float speed = 1;
+    Rigidbody2D rb;
+
     void Start()
     {
-        
+        player = GameObject.FindGameObjectWithTag("Player").GetComponent<Transform>();
+        rb = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    void Following()
+    {
+        Vector3 dir = (player.transform.position - transform.position).normalized;
+        Vector3 deltaPosition = speed * dir * Time.deltaTime;
+        rb.MovePosition(transform.position + deltaPosition);
+    }
+
+    void Explode()
+    {
+
     }
 }
